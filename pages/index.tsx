6cf310ea022abccd28d4136fd747249f581e3408
TranPhuongDinh <dinhblade123@gmail.com>
2022-12-18 01:33:06 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Container, Header, Main, Footer, Cards } from "@components";
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            <button></button>
+            <Header />
+            <Main />
+            <Cards />
+            <Footer />
+        </Container>
+    );
+};
+
+export default Home;
